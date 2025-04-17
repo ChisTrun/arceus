5918ba007d836109215e2b59a1159bb0e1619c5f
chistrun <vochitrung.pst@gmail.com>
2025-04-17 21:07:29 +0700

    update

--- internal/server/arceus/grpc_arceus_generatetext.go
+++ internal/server/arceus/grpc_arceus_generatetext.go
@@ -2,12 +2,17 @@ package arceus
 
 import (
 	"context"
+	"fmt"
 
 	arceus "arceus/api"
+	"arceus/pkg/logger/pkg/logging"
 )
 
 func (s *arceusServer) GenerateText(ctx context.Context, request *arceus.GenerateTextRequest) (*arceus.GenerateTextResponse, error) {
+	logging.Logger(ctx).Info("Received GenerateText request")
+
 	if err := request.Validate(); err != nil {
+		logging.Logger(ctx).Error(fmt.Sprintf("Error: %v", err.Error()))
 		return nil, err
 	}
 
